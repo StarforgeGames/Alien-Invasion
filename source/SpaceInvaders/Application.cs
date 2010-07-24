fcacc158438ae18c568cc08c2cf5d30f50242f6f
Michael Barth <Michael Barth@localhost>
2010-07-24 10:10:46 +0200

    Added source with GameTimer and basic DX10 setup Added release build

---
+++ source/SpaceInvaders/Application.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvaders
+{
+    /// <summary>
+    /// Concrete class for the game.
+    /// </summary>
+    class Application : Dx10Application
+    {
+        public Application()
+        {
+            this.GameTitle = "Space Invaders";
+        }
+
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+
+            int deltaMilliseconds = (int) deltaTime * 1000;
+        }
+    }
+}
---
+++ source/SpaceInvaders/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Dx10Application app = new Application();
+            app.Initialize();
+            app.Run();
+        }
+    }
+}
