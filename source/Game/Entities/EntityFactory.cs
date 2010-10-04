2b794b945f9f400d6f7bf834959479b74eece1f4
Michael Barth <Michael Barth@localhost>
2010-10-04 22:02:13 +0200

    Added a rocking "pewpew" laser projectile graphic, yeah! 8)

--- source/Game/Entities/EntityFactory.cs
+++ source/Game/Entities/EntityFactory.cs
@@ -20,14 +20,16 @@ namespace Game.Entities
         {
             switch (id.ToLowerInvariant()) {
                 case "player":
-                    Entity player = new Actor(game, id, @"Gfx\player.png", 50f, 50f, 50f);
+                    float x = game.WorldWidth / 2;
+                    float y = game.WorldHeight - 100;
+                    Entity player = new Actor(game, id, @"Gfx\player.png", x, y, 50f);
                     player.AddBehaviour(new CombatBehaviour(player, .75f));
                     player.AddBehaviour(new CollisionBehaviour(player));
                     player.AddBehaviour(new HealthBehaviour(player, 1));
                     return player;
 
                 case "pewpew":
-                    Entity pewpew = new Actor(game, id, @"Gfx\player.png", 50f, 50f, 50f);
+                    Entity pewpew = new Actor(game, id, @"Gfx\pewpew.png", 0f, 0f, 200f);
                     pewpew.AddBehaviour(new CollisionBehaviour(pewpew));
                     return pewpew;
 
