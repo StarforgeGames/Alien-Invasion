80c5b4c4baadd69521a4258871c9b5df997e5aee
patrigg <patrigg@localhost>
2010-10-18 23:00:29 +0200

    added extractor class

---
+++ source/Graphics/Extractor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphics
+{
+    public class Extractor
+    {
+        public bool Extract 
+        {
+            private get;
+            public set;
+        }
+        
+
+        public void Run()
+        {
+            if (Extract)
+            {
+
+            }
+        }
+    }
+}
