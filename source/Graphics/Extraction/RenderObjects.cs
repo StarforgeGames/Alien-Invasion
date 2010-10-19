f4c8e27b44029ada2a42023c8d487c472dad52df
patrigg <patrigg@localhost>
2010-10-19 09:44:54 +0200

    extracted RenderObject management in seperate class

---
+++ source/Graphics/Extraction/RenderObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Graphics.ResourceManagement;
+
+namespace Graphics
+{
+    class RenderObjects
+    {
+        private Dictionary<ResourceHandle, Dictionary<ResourceHandle, RenderObjects>> objs;
+
+        internal void Clear()
+        {
+            objs.Clear();
+        }
+    }
+}
