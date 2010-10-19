c90406e76b533646d19f380ea4f788b765103828
Michael Barth <Michael Barth@localhost>
2010-10-19 23:30:48 +0200

    Added AttributeLoader Added static Attribute Parsers Shortened "default.material" to "default.mtl" Customised RenderBehavior Attributes

---
+++ source/Game/Entities/AttributeLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using Game.Entities.AttributeParser;
+
+namespace Game.Entities
+{
+
+    static class AttributeLoader
+    {
+        private delegate object Parser(XmlNode node);
+        private static Dictionary<string, Parser> loader = new Dictionary<string, Parser>();
+
+        static AttributeLoader()
+        {
+            loader.Add(Vector2DParser.Type, Vector2DParser.Parse);
+        }
+        
+        public static object Load(XmlNode node)
+        {
+            if (!loader.ContainsKey(node.Name)) {
+                throw new NotSupportedException("No loader for attribute type: '" + node.Name + "' available");
+            }
+
+            return loader[node.Name].Invoke(node);
+        }
+    }
+
+}
