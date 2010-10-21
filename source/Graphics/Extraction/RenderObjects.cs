681fe5affbab0fafee7d0e95b04a9cc2948207e2
patrigg <patrigg@localhost>
2010-10-21 09:23:33 +0200

    Extracted resource management from graphics assembly moved loaders which work on graphics resources to graphics assembly

--- source/Graphics/Extraction/RenderObjects.cs
+++ source/Graphics/Extraction/RenderObjects.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Graphics.ResourceManagement;
+using ResourceManagement;
 using SlimDX;
 
 namespace Graphics
---
+++ source/Graphics/Resources/EffectResource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX.Direct3D10;
+using ResourceManagement.Resources;
+
+namespace Graphics.Resources
+{
+    public class EffectResource : AResource
+    {
+        public string errors;
+        public Effect effect;
+    }
+}
