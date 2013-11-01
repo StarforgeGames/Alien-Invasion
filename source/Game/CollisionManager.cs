3579f9fc268d53eb04b9fba448d063d2442aa89a
Michael Barth <Michael Barth@localhost>
2013-11-01 23:10:11 +0100

    Fixed bug with starting second new game Laser shots now ignore each other for collision (it's no longer possible to shoot laser shots) Added hammerhead and ray idle animations

--- source/Game/CollisionManager.cs
+++ source/Game/CollisionManager.cs
@@ -12,117 +12,120 @@ using System.Diagnostics;
 
 namespace Game
 {
-    public class CollisionManager : IEventListener
-    {
-        public GameLogic Game { get; private set; }
-        public IEventManager EventManager { get; private set; }
+	public class CollisionManager : IEventListener
+	{
+		public GameLogic Game { get; private set; }
+		public IEventManager EventManager { get; private set; }
 
-        private List<Entity> collidables;
+		private List<Entity> collidables;
 
 
-        public CollisionManager(GameLogic game)
-        {
-            this.Game = game;
-            this.EventManager = game.EventManager;
+		public CollisionManager(GameLogic game)
+		{
+			this.Game = game;
+			this.EventManager = game.EventManager;
 
-            this.collidables = new List<Entity>();
+			this.collidables = new List<Entity>();
 
-            registerGameEventListeners();
-        }
+			registerGameEventListeners();
+		}
 
-        private void registerGameEventListeners()
-        {
-            EventManager.AddListener(this, typeof(NewEntityEvent));
-            EventManager.AddListener(this, typeof(DestroyEntityEvent));
-        }
+		private void registerGameEventListeners()
+		{
+			EventManager.AddListener(this, typeof(NewEntityEvent));
+			EventManager.AddListener(this, typeof(DestroyEntityEvent));
+		}
 
-        public void DetectAndResolveCollisions()
-        {
-            foreach (Entity entity in collidables) {
-                if (entity.IsDead) {
-                    continue;
-                }
+		public void DetectAndResolveCollisions()
+		{
+			foreach (Entity entity in collidables) {
+				if (entity.IsDead) {
+					continue;
+				}
 
-                foreach (Entity other in collidables) {
-                    if (entity.ID == other.ID || other.IsDead) {
-                        continue;
-                    }
+				foreach (Entity other in collidables) {
+					if (entity.ID == other.ID 
+						|| other.IsDead
+						|| (entity.Type.StartsWith("pewpew") && other.Type.StartsWith("pewpew")))
+					{
+						continue;
+					}
 
-                    var entityFaction = entity[CollisionBehavior.Key_Faction];
-                    var otherFaction = other[CollisionBehavior.Key_Faction];
-                    // No friendly fire... or collision
-                    if (entityFaction == otherFaction) {
-                        continue;
-                    }
+					var entityFaction = entity[CollisionBehavior.Key_Faction];
+					var otherFaction = other[CollisionBehavior.Key_Faction];
+					// No friendly fire... or collision
+					if (entityFaction == otherFaction) {
+						continue;
+					}
 
-                    if (AreColliding(entity, other)) {
-                        Console.WriteLine("[" + this.GetType().Name + "] " + entity.Type + " collided with " 
-                            + other.Type + "!");
+					if (AreColliding(entity, other)) {
+						Console.WriteLine("[" + this.GetType().Name + "] " + entity.Type + " collided with " 
+							+ other.Type + "!");
 
-                        CollisionEvent collisionMsg = CollisionEvent.Collides(other.ID, entity.ID);
-                        EventManager.Queue(collisionMsg);
+						CollisionEvent collisionMsg = CollisionEvent.Collides(other.ID, entity.ID);
+						EventManager.Queue(collisionMsg);
 
-                        int collisionDmg = entity[CollisionBehavior.Key_CollisionDamage];
-                        DamageEvent dmgMsg = DamageEvent.Receive(collisionDmg, entity.ID, other.ID);
-                        EventManager.Queue(dmgMsg);
-                    }
-                }
-            }
-            
-        }
-        
-        public void Reset()
-        {
-            collidables.Clear();
-        }
+						int collisionDmg = entity[CollisionBehavior.Key_CollisionDamage];
+						DamageEvent dmgMsg = DamageEvent.Receive(collisionDmg, entity.ID, other.ID);
+						EventManager.Queue(dmgMsg);
+					}
+				}
+			}
+			
+		}
+		
+		public void Reset()
+		{
+			collidables.Clear();
+		}
 
-        public bool AreColliding(Entity entity, Entity other)
-        {
-            Vector2D position = entity[SpatialBehavior.Key_Position];
-            Vector2D dimensions = entity[SpatialBehavior.Key_Dimensions];
+		public bool AreColliding(Entity entity, Entity other)
+		{
+			Vector2D position = entity[SpatialBehavior.Key_Position];
+			Vector2D dimensions = entity[SpatialBehavior.Key_Dimensions];
 
-            Vector2D otherPosition = other[SpatialBehavior.Key_Position];
-            Vector2D otherDimensions = other[SpatialBehavior.Key_Dimensions];
+			Vector2D otherPosition = other[SpatialBehavior.Key_Position];
+			Vector2D otherDimensions = other[SpatialBehavior.Key_Dimensions];
 
-            return position.X <= otherPosition.X + otherDimensions.X
-                && position.Y <= otherPosition.Y + otherDimensions.Y
-                && position.X + dimensions.X >= otherPosition.X
-                && position.Y + dimensions.Y >= otherPosition.Y;
-        }
+			return position.X <= otherPosition.X + otherDimensions.X
+				&& position.Y <= otherPosition.Y + otherDimensions.Y
+				&& position.X + dimensions.X >= otherPosition.X
+				&& position.Y + dimensions.Y >= otherPosition.Y;
+		}
 
-        public void OnEvent(Event evt)
-        {
-            switch (evt.Type) {
-                case NewEntityEvent.NEW_ENTITY: {
-                        NewEntityEvent newEntityEvent = (NewEntityEvent)evt;
-                        Entity entity = Game.World.Entities[newEntityEvent.EntityID];
-                        OnAttach(entity);
-                        break;
-                    }
-                case DestroyEntityEvent.DESTROY_ENTITY: {
-                        DestroyEntityEvent destroyEntityEvent = (DestroyEntityEvent)evt;
-                        Entity entity = Game.World.Entities[destroyEntityEvent.EntityID];
-                        OnDetach(entity);
-                        break;
-                    }
-            }
-        }
+		public void OnEvent(Event evt)
+		{
+			switch (evt.Type) {
+				case NewEntityEvent.NEW_ENTITY: {
+						NewEntityEvent newEntityEvent = (NewEntityEvent)evt;
+						Entity entity = Game.World.Entities[newEntityEvent.EntityID];
+						OnAttach(entity);
+						break;
+					}
+				case DestroyEntityEvent.DESTROY_ENTITY: {
+						DestroyEntityEvent destroyEntityEvent = (DestroyEntityEvent)evt;
+						Entity entity = Game.World.Entities[destroyEntityEvent.EntityID];
+						OnDetach(entity);
+						break;
+					}
+			}
+		}
 
-        public void OnAttach(Entity entity)
-        {
-            if (entity.HasBehavior(typeof(CollisionBehavior)))
-            {
-                // Log.Error("Adding " + entity.ToString() + "to collidables.", this.GetType().Name);
-                collidables.Add(entity);
-            }
-        }
+		public void OnAttach(Entity entity)
+		{
+			if (entity.HasBehavior(typeof(CollisionBehavior)))
+			{
+				// Log.Error("Adding " + entity.ToString() + "to collidables.", this.GetType().Name);
+				collidables.Add(entity);
+			}
+		}
 
-        public void OnDetach(Entity entity)
-        {
-            if (collidables.Contains(entity)) {
-                //Log.Error("Removing " + entity.ToString() + "from collidables.", this.GetType().Name);
-                collidables.Remove(entity);
-            }
-        }
-    }
+		public void OnDetach(Entity entity)
+		{
+			if (collidables.Contains(entity)) {
+				//Log.Error("Removing " + entity.ToString() + "from collidables.", this.GetType().Name);
+				collidables.Remove(entity);
+			}
+		}
+	}
 }
--- source/SpaceInvaders/Views/AiView.cs
+++ source/SpaceInvaders/Views/AiView.cs
@@ -9,236 +9,237 @@ using Game.Behaviors;
 
 namespace SpaceInvaders.Views
 {
-    class AiView : IGameView
-    {
-        public GameLogic Game { get; private set; }
-        public IEventManager EventManager { get; private set; }
+	class AiView : IGameView
+	{
+		public GameLogic Game { get; private set; }
+		public IEventManager EventManager { get; private set; }
 
-        public GameViewType Type { get { return GameViewType.PlayerView; } }
-        public int ID
-        {
-            get { throw new NotImplementedException(); }
-        }
+		public GameViewType Type { get { return GameViewType.PlayerView; } }
+		public int ID
+		{
+			get { throw new NotImplementedException(); }
+		}
 
-        private List<Entity> invaders = new List<Entity>();
-        private Vector2D currentDirection;
+		private List<Entity> invaders = new List<Entity>();
+		private Vector2D currentDirection;
 
-        private float moveDownTime;
-        private float totalMoveDownTime = 0.5f;
+		private float moveDownTime;
+		private float totalMoveDownTime = 0.5f;
 
-        private bool movementDirectionChanged = false;
+		private bool movementDirectionChanged = false;
 
-        private float timeSinceLastShot = 0.0f;
-        private Entity shooter;
-        private Random rng = new Random();
+		private float timeSinceLastShot = 0.0f;
+		private Entity shooter;
+		private Random rng = new Random();
 
 
-        private Entity mysteryShip;
-        private readonly int minMysteryShipSpawnTime = 2;
-        private readonly int maxMysteryShipSpawnTime = 15;
-        private float timeSinceLastMysteryShipSpawn;
-        private float timeToNextMysteryShipSpawn;
+		private Entity mysteryShip;
+		private readonly int minMysteryShipSpawnTime = 2;
+		private readonly int maxMysteryShipSpawnTime = 15;
+		private float timeSinceLastMysteryShipSpawn;
+		private float timeToNextMysteryShipSpawn;
 
-        private readonly float speedIncreasePerDownTurn = 0.03f;
-        private readonly float firingRateDecreasePerDownTurn = 0.01f;
+		private readonly float speedIncreasePerDownTurn = 0.03f;
+		private readonly float firingRateDecreasePerDownTurn = 0.01f;
 
-        public AiView(GameLogic game)
-        {
-            this.Game = game;
-            this.EventManager = game.EventManager;
+		public AiView(GameLogic game)
+		{
+			this.Game = game;
+			this.EventManager = game.EventManager;
 
-            this.timeToNextMysteryShipSpawn = minMysteryShipSpawnTime + rng.Next(maxMysteryShipSpawnTime);
+			this.timeToNextMysteryShipSpawn = minMysteryShipSpawnTime + rng.Next(maxMysteryShipSpawnTime);
 
-            registerGameEventListeners();
-        }
+			registerGameEventListeners();
+		}
 
-        private void registerGameEventListeners()
-        {
-            EventManager.AddListener(this, typeof(NewEntityEvent));
-            EventManager.AddListener(this, typeof(DestroyEntityEvent));
-            EventManager.AddListener(this, typeof(GameStateChangedEvent));
-            EventManager.AddListener(this, typeof(AiUpdateMovementEvent));
-        }
+		private void registerGameEventListeners()
+		{
+			EventManager.AddListener(this, typeof(NewEntityEvent));
+			EventManager.AddListener(this, typeof(DestroyEntityEvent));
+			EventManager.AddListener(this, typeof(GameStateChangedEvent));
+			EventManager.AddListener(this, typeof(AiUpdateMovementEvent));
+		}
 
-        public void OnUpdate(float deltaTime)
-        {
-            if (Game.IsPaused || invaders.Count < 1) 
-            {
-                return;
-            }
+		public void OnUpdate(float deltaTime)
+		{
+			if (Game.IsPaused || invaders.Count < 1) 
+			{
+				return;
+			}
 
-            handleShooting(deltaTime);
-            handleMovement(deltaTime);
-            handleMysteryShip(deltaTime);
-        }
+			handleShooting(deltaTime);
+			handleMovement(deltaTime);
+			handleMysteryShip(deltaTime);
+		}
 
-        private void handleShooting(float deltaTime)
-        {
-            timeSinceLastShot += deltaTime;
-            shooter = shooter ?? invaders[rng.Next(invaders.Count)];
-            float firingSpeed = shooter[CombatBehavior.Key_FiringSpeed];
+		private void handleShooting(float deltaTime)
+		{
+			timeSinceLastShot += deltaTime;
+			shooter = shooter ?? invaders[rng.Next(invaders.Count)];
+			float firingSpeed = shooter[CombatBehavior.Key_FiringSpeed];
 
-            if (timeSinceLastShot + rng.NextDouble() >= firingSpeed) 
-            {
-                EventManager.Queue(FireWeaponEvent.SingleShot(shooter.ID));
+			if (timeSinceLastShot + rng.NextDouble() >= firingSpeed) 
+			{
+				EventManager.Queue(FireWeaponEvent.SingleShot(shooter.ID));
 
-                timeSinceLastShot = 0.0f;
-                shooter = null;
-            }
-        }
+				timeSinceLastShot = 0.0f;
+				shooter = null;
+			}
+		}
 
-        private void handleMovement(float deltaTime)
-        {
-            if (currentDirection.Y < 0) 
-            {
-                if (moveDownTime >= totalMoveDownTime) 
-                {
-                    currentDirection.Y = 0.0f;
-                    moveDownTime = 0.0f;
-                    movementDirectionChanged = true;
-                }
+		private void handleMovement(float deltaTime)
+		{
+			if (currentDirection.Y < 0) 
+			{
+				if (moveDownTime >= totalMoveDownTime) 
+				{
+					currentDirection.Y = 0.0f;
+					moveDownTime = 0.0f;
+					movementDirectionChanged = true;
+				}
 
-                moveDownTime += deltaTime;
-            }
+				moveDownTime += deltaTime;
+			}
 
-            if (!movementDirectionChanged) 
-            {
-                return;
-            }
+			if (!movementDirectionChanged) 
+			{
+				return;
+			}
 
-            foreach (Entity invader in invaders) {
-                var move = MoveEvent.Start(invader.ID, currentDirection);
-                EventManager.Queue(move);
+			foreach (Entity invader in invaders) {
+				var move = MoveEvent.Start(invader.ID, currentDirection);
+				EventManager.Queue(move);
 
-                invader[CombatBehavior.Key_FiringSpeed] -= firingRateDecreasePerDownTurn;
-                invader[SpatialBehavior.Key_MovementSpeed] *= 1 + speedIncreasePerDownTurn;              
-            }
-            totalMoveDownTime /= 1 + speedIncreasePerDownTurn;
-            movementDirectionChanged = false;
-        }
+				invader[CombatBehavior.Key_FiringSpeed] -= firingRateDecreasePerDownTurn;
+				invader[SpatialBehavior.Key_MovementSpeed] *= 1 + speedIncreasePerDownTurn;              
+			}
+			totalMoveDownTime /= 1 + speedIncreasePerDownTurn;
+			movementDirectionChanged = false;
+		}
 
-        private void handleMysteryShip(float deltaTime)
-        {
-            if (mysteryShip != null)
-            {
-                return;
-            }
+		private void handleMysteryShip(float deltaTime)
+		{
+			if (mysteryShip != null)
+			{
+				return;
+			}
 
-            timeSinceLastMysteryShipSpawn += deltaTime;
-            if (timeSinceLastMysteryShipSpawn >= timeToNextMysteryShipSpawn) 
-            {
-                var createEntity = CreateEntityEvent.New("mystery_ship");
+			timeSinceLastMysteryShipSpawn += deltaTime;
+			if (timeSinceLastMysteryShipSpawn >= timeToNextMysteryShipSpawn) 
+			{
+				var createEntity = CreateEntityEvent.New("mystery_ship");
 
-                bool isSpawningOnTheRight = rng.Next(2) == 1;
+				bool isSpawningOnTheRight = rng.Next(2) == 1;
 
-                if (isSpawningOnTheRight) 
-                {
-                    Vector2D position = new Vector2D(Game.World.Width, Game.World.Height - 100);
-                    createEntity.AddAttribute(SpatialBehavior.Key_Position, position);
-                    Vector2D direction = new Vector2D(-1, 0);
-                    createEntity.AddAttribute(SpatialBehavior.Key_MoveDirection, direction);
-                }
-                else 
-                {
-                    Vector2D position = new Vector2D(-70, Game.World.Height - 100);
-                    createEntity.AddAttribute(SpatialBehavior.Key_Position, position);
-                    Vector2D direction = new Vector2D(1, 0);
-                    createEntity.AddAttribute(SpatialBehavior.Key_MoveDirection, direction);
-                }
-                
+				if (isSpawningOnTheRight) 
+				{
+					Vector2D position = new Vector2D(Game.World.Width, Game.World.Height - 100);
+					createEntity.AddAttribute(SpatialBehavior.Key_Position, position);
+					Vector2D direction = new Vector2D(-1, 0);
+					createEntity.AddAttribute(SpatialBehavior.Key_MoveDirection, direction);
+				}
+				else 
+				{
+					Vector2D position = new Vector2D(-70, Game.World.Height - 100);
+					createEntity.AddAttribute(SpatialBehavior.Key_Position, position);
+					Vector2D direction = new Vector2D(1, 0);
+					createEntity.AddAttribute(SpatialBehavior.Key_MoveDirection, direction);
+				}
+				
 
-                EventManager.Queue(createEntity);
+				EventManager.Queue(createEntity);
 
-                timeSinceLastMysteryShipSpawn = 0.0f;
-                timeToNextMysteryShipSpawn = minMysteryShipSpawnTime + rng.Next(maxMysteryShipSpawnTime);
-            }
-        }
+				timeSinceLastMysteryShipSpawn = 0.0f;
+				timeToNextMysteryShipSpawn = minMysteryShipSpawnTime + rng.Next(maxMysteryShipSpawnTime);
+			}
+		}
 
-        public void OnEvent(Event evt)
-        {
-            switch (evt.Type) {
-                case NewEntityEvent.NEW_ENTITY: {
-                    var newEntityEvent = (NewEntityEvent)evt;
-                    Entity entity = Game.World.Entities[newEntityEvent.EntityID];
-                    if (entity.Type.StartsWith("alien_")) 
-                    {
-                        OnAttach(entity);
-                    }
-                    else if (entity.Type.Equals("mystery_ship")) 
-                    {
-                        mysteryShip = entity;
-                    }
-                    break;
-                }
-                case DestroyEntityEvent.DESTROY_ENTITY: {
-                    var destroyEntityEvent = (DestroyEntityEvent)evt;
-                   
-                    Entity entity = Game.World.Entities[destroyEntityEvent.EntityID];
-                    if (entity.Type.StartsWith("alien_")) 
-                    {
-                        OnDetach(entity);
-                    }
-                    else if (entity.Type.Equals("mystery_ship")) 
-                    {
-                        mysteryShip = null;
+		public void OnEvent(Event evt)
+		{
+			switch (evt.Type) {
+				case NewEntityEvent.NEW_ENTITY: {
+					var newEntityEvent = (NewEntityEvent)evt;
+					Entity entity = Game.World.Entities[newEntityEvent.EntityID];
+					if (entity.Type.StartsWith("alien_")) 
+					{
+						OnAttach(entity);
+					}
+					else if (entity.Type.Equals("mystery_ship")) 
+					{
+						mysteryShip = entity;
+					}
+					break;
+				}
+				case DestroyEntityEvent.DESTROY_ENTITY: {
+					var destroyEntityEvent = (DestroyEntityEvent)evt;
+				   
+					Entity entity = Game.World.Entities[destroyEntityEvent.EntityID];
+					if (entity.Type.StartsWith("alien_")) 
+					{
+						OnDetach(entity);
+					}
+					else if (entity.Type.Equals("mystery_ship")) 
+					{
+						mysteryShip = null;
 
-                    }
+					}
 
-                    if (entity.ID == shooter.ID)
-                    {
-                        shooter = null;
-                    }
-                    break;
-                }
-                case GameStateChangedEvent.GAME_STATE_CHANGED: {
-                    var stateChangedEvent = (GameStateChangedEvent)evt;
-                    onGameStateChanged(stateChangedEvent.NewState);
-                    break;
-                    }
-                case AiUpdateMovementEvent.AT_BORDER: {
-                    var aiMovementUpdateEvent = (AiUpdateMovementEvent)evt;
+					if (entity.ID == shooter.ID)
+					{
+						shooter = null;
+					}
+					break;
+				}
+				case GameStateChangedEvent.GAME_STATE_CHANGED: {
+					var stateChangedEvent = (GameStateChangedEvent)evt;
+					onGameStateChanged(stateChangedEvent.NewState);
+					break;
+					}
+				case AiUpdateMovementEvent.AT_BORDER: {
+					var aiMovementUpdateEvent = (AiUpdateMovementEvent)evt;
 
-                    Vector2D borderData = aiMovementUpdateEvent.BorderData;
+					Vector2D borderData = aiMovementUpdateEvent.BorderData;
 
-                    if (borderData.Y < 0) 
-                    {
-                        // Victory for the Invaders!
-                        EventManager.Trigger(GameStateChangedEvent.To(GameState.GameOver));
-                    }
+					if (borderData.Y < 0) 
+					{
+						// Victory for the Invaders!
+						EventManager.Trigger(GameStateChangedEvent.To(GameState.GameOver));
+					}
 
-                    currentDirection.X = -borderData.X;
-                    currentDirection.Y = -1;
+					currentDirection.X = -borderData.X;
+					currentDirection.Y = -1;
 
-                    movementDirectionChanged = true;
-                    break;
-                }
-            }
-        }
+					movementDirectionChanged = true;
+					break;
+				}
+			}
+		}
 
-        public void OnAttach(Entity entity)
-        {
-            invaders.Add(entity);
-        }
+		public void OnAttach(Entity entity)
+		{
+			invaders.Add(entity);
+		}
 
-        public void OnDetach(Entity entity)
-        {
-            if (invaders.Contains(entity)) {
-                invaders.Remove(entity);
-            }
-        }
+		public void OnDetach(Entity entity)
+		{
+			if (invaders.Contains(entity)) {
+				invaders.Remove(entity);
+			}
+		}
 
-        private void onGameStateChanged(GameState newState)
-        {
-            switch (newState) {               
-                case GameState.Loading:
-                    currentDirection = new Vector2D(1, 0);
-                    movementDirectionChanged = true;
-                    invaders.Clear();
-                    break;              
-                default:
-                    break;
-            }
-        }
+		private void onGameStateChanged(GameState newState)
+		{
+			switch (newState) {
+				case GameState.Loading:
+					invaders.Clear();
+					shooter = null;
+					currentDirection = new Vector2D(1, 0);
+					movementDirectionChanged = true;
+					break;              
+				default:
+					break;
+			}
+		}
 
-    }
+	}
 }
