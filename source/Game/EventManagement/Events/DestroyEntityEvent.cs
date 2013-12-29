bea7a9848a00d91fa9f885e0b7729665965b4352
Michael Barth <Michael Barth@localhost>
2013-12-29 15:18:13 +0100

    Bugfix for mystery ship destroy sound: Now only plays when destroyed by player

--- source/Game/EventManagement/Events/DestroyEntityEvent.cs
+++ source/Game/EventManagement/Events/DestroyEntityEvent.cs
@@ -1,25 +1,27 @@
 ﻿namespace Game.EventManagement.Events
 {
 
-    public class DestroyEntityEvent : Event
-    {
-        // Event Message Types
-        public const string DESTROY_ENTITY = "DESTROY_ENTITY";
+	public class DestroyEntityEvent : Event
+	{
+		// Event Message Types
+		public const string DESTROY_ENTITY = "DESTROY_ENTITY";
 
-        public int EntityID { get; private set; }
-        public int DestroyedByEntityID { get; private set; }
+		public int EntityID { get; private set; }
+		public int DestroyedByEntityID { get; private set; }
+		public bool IsCausedByCleanup { get; private set; }
 
-        public DestroyEntityEvent(string type, int entityID, int destroyedByEntityID = 0)
-            : base(type)
-        {
-            this.EntityID = entityID;
-            this.DestroyedByEntityID = destroyedByEntityID;
-        }
+		public DestroyEntityEvent(string type, int entityID, int destroyedByEntityID = 0, bool causedByCleanup = false)
+			: base(type)
+		{
+			this.EntityID = entityID;
+			this.DestroyedByEntityID = destroyedByEntityID;
+			this.IsCausedByCleanup = causedByCleanup;
+		}
 
-        public static DestroyEntityEvent Destroy(int entityID, int destroyedByEntityID = 0)
-        {
-            return new DestroyEntityEvent(DESTROY_ENTITY, entityID, destroyedByEntityID);
-        }
-    }
+		public static DestroyEntityEvent Destroy(int entityID, int destroyedByEntityID = 0, bool causedByCleanup = false)
+		{
+			return new DestroyEntityEvent(DESTROY_ENTITY, entityID, destroyedByEntityID, causedByCleanup);
+		}
+	}
 
 }
